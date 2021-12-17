0714bcc4da108420da23fc5a1e38bcc2a6b89931
Bacco40 <bacchettimatteo@gmail.com>
2021-12-17 17:58:58 +0100

    changed the javascript to make it listen to the user selection using a button and putted the results directly on the screen!

--- script.js
+++ script.js
@@ -1,56 +1,70 @@
-function game(playerScore,computerScore) {
-    while(playerScore<5 && computerScore<5){
-        let playerSelection= String(prompt("Rock, Paper or Scissor?")).toLowerCase();
-        if(playerSelection!="rock" && playerSelection!="paper" && playerSelection!="scissor"){
-            alert("Are you stupid?");
-        }
-        else{
-
-            function randomSelection(){
-                let s="";
-                let x= Math.floor(Math.random() * 3)+1;
-                if(x==1){
-                    s="rock";
-                }else if(x==2){
-                    s="paper";
-                }else if(x==3){
-                    s="scissor";
-                }
-                return s;
-            }
-            let computerSelection= randomSelection();
-
-            function playRound(playerSelection,computerSelection){
-                let Win="You Win! ";
-                let Lose="You Lose! ";
-                let Tie="It's a Tie! Both of you choosed ";
-                if(playerSelection==computerSelection){
-                    Tie=Tie+playerSelection;
-                    return(Tie);
-                }
-                else if(playerSelection=="rock"&&computerSelection=="scissor"||playerSelection=="scissor"&&computerSelection=="paper"||playerSelection=="paper"&&computerSelection=="rock"){
-                    Win=Win+playerSelection+" beats "+computerSelection;
-                    playerScore=playerScore+1;
-                    return(Win);
-                }
-                else if(playerSelection=="rock"&&computerSelection=="paper"||playerSelection=="scissor"&&computerSelection=="rock"||playerSelection=="paper"&&computerSelection=="scissor"){
-                    Lose=Lose+computerSelection+" beats "+playerSelection;
-                    computerScore=computerScore+1;
-                    return(Lose)
-                }
-            }
-
-            console.log(playRound(playerSelection,computerSelection));
-            console.log("the score is "+playerScore+"-"+computerScore);
-        }
-    }
-    if(playerScore>computerScore){
-        return("You won the game! Congratulations!");
-    }else{
-        return("Unortunately you lost the game! Computer are better than human");
-    }
-}
-
 let playerScore=0;
 let computerScore=0;
-alert(game(playerScore,computerScore));
+const results=document.querySelector('.Results');
+const points=document.querySelector('#Points');
+const humanPoints= document.createElement('div');
+const computerPoints= document.createElement('div');
+const output=document.createElement('div');
+humanPoints.textContent = playerScore;
+computerPoints.textContent = computerScore;
+results.appendChild(output);
+points.appendChild(humanPoints);
+points.appendChild(computerPoints);
+let playerSelection=""; 
+function randomSelection(){
+    let s="";
+    let x= Math.floor(Math.random() * 3)+1;
+    if(x==1){
+        s="rock";
+    }else if(x==2){
+        s="paper";
+    }else if(x==3){
+        s="scissor";
+    }
+    return s;
+}
+function playRound(playerSelection,computerSelection){
+    let Win="You Win! ";
+    let Lose="You Lose! ";
+    let Tie="It's a Tie! Both of you choosed ";
+    if(playerSelection==computerSelection){
+        Tie=Tie+playerSelection;
+        return(Tie);
+    }
+    else if(playerSelection=="rock"&&computerSelection=="scissor"||playerSelection=="scissor"&&computerSelection=="paper"||playerSelection=="paper"&&computerSelection=="rock"){
+        Win=Win+playerSelection+" beats "+computerSelection;
+        playerScore=playerScore+1;
+        humanPoints.textContent = playerScore;
+        if(playerScore==5){
+            playerScore=0;
+            computerScore=0;
+            computerPoints.textContent = computerScore;
+            humanPoints.textContent = playerScore;
+            alert("Congratulations you Won the game!")
+        }
+        return(Win);
+    }
+    else if(playerSelection=="rock"&&computerSelection=="paper"||playerSelection=="scissor"&&computerSelection=="rock"||playerSelection=="paper"&&computerSelection=="scissor"){
+        Lose=Lose+computerSelection+" beats "+playerSelection;
+        computerScore=computerScore+1;
+        computerPoints.textContent = computerScore;
+        if(computerScore==5){
+            playerScore=0;
+            computerScore=0;
+            computerPoints.textContent = computerScore;
+            humanPoints.textContent = playerScore;
+            alert("Machines Won!!! Humanity is doomed! AH LOSERRR!!")
+        }
+        return(Lose)
+    }
+}
+let buttons = document.querySelectorAll('button');
+buttons.forEach((button)=>{
+    button.addEventListener('click',()=>{
+    let playerSelection=button.id;
+    let computerSelection= randomSelection();
+    output.textContent=(playRound(playerSelection,computerSelection)); 
+    })
+})
+
+
